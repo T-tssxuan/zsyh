1f74552ddef8dcf481615d52a7565213abd959dd
luoxuan01 <T-tssxuan>
2017-05-05 19:49:49 +0800

    [new] add the graph generate file

--- code/gbrt.py
+++ code/gbrt.py
@@ -1,7 +1,11 @@
+# Author: Luo Xuan
+# This file is the traditional regression algorithm based on the gbrt algrithm
+# Dependency: numpy, scikit-learn, pandas
+# Run: python3 gbrt.py
+
 import numpy as np
 import sklearn as sk
 import pandas as pd
-# import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import GradientBoostingRegressor
 
@@ -19,29 +23,29 @@ Y = predict.as_matrix().reshape(-1)
 X_train, X_test = X[:3300], X[3300:]
 y_train, y_test = Y[:3300], Y[3300:]
 
-
+# Construct the keys
 tags = [
         'TIPS-5Y', 'TIPS-10Y', 'TIPS-20Y', 'TIPS-30Y', 'TIPS-LONG', 
         'UST BILL 1-Y RETURN', 'UST BILL 10-Y RETURN', 'SP500', 
         'LIBOR-OVERNIGHT', 'COMEX-NC-LONG', 'COMEX-NC-NET', 'SPDR-t',
         'USD/CNY'
         ]
-
-
+# Form the single feature regression
 pairs_single = {
         tags[0]: (0,), tags[1]: (1,), tags[2]: (2,), tags[3]: (3,), tags[4]: (4,), 
         tags[5]: (5,), tags[6]: (6,), tags[7]: (7,), tags[8]: (8,), tags[9]: (9,), 
         tags[10]: (10,), tags[11]: (11,), tags[12]: (12,)
         }
+# Form the multi feature regression
 pairs_compose = {
-        # '+'.join([tags[0], tags[7]]): (0, 7),
-        # '+'.join([tags[0], tags[7]]): (0, 7),
-        # '+'.join([tags[0], tags[5], tags[7]]): (0, 5, 7),
-        # '+'.join([tags[0], tags[5], tags[7], tags[12]]): (0, 5, 7, 12),
+        '+'.join([tags[0], tags[7]]): (0, 7),
+        '+'.join([tags[0], tags[5], tags[7]]): (0, 5, 7),
+        '+'.join([tags[0], tags[5], tags[7], tags[12]]): (0, 5, 7, 12),
         '+'.join([tags[0], tags[5], tags[7], tags[9], tags[12]]): (0, 5, 7, 9, 12)
-        # '+'.join(tags): tuple(i for i in range(len(tags)))
+        '+'.join(tags): tuple(i for i in range(len(tags)))
         }
 
+# The predicton function to predict the result
 def predict(name, idx, epoch, lr):
     X_train_tmp, X_test_tmp = X_train[:, idx], X_test[:, idx]
     est = GradientBoostingRegressor(n_estimators=epoch, learning_rate=lr, 
@@ -49,8 +53,8 @@ def predict(name, idx, epoch, lr):
     loss = mean_squared_error(y_test, est.predict(X_test_tmp)) 
     print('{} L2 loss: {}'.format(name, loss))
 
-# for key in pairs_single:
-#     predict(key, pairs_single[key], 10000, 0.1)
+for key in pairs_single:
+    predict(key, pairs_single[key], 10000, 0.1)
 
 for key in pairs_compose:
     predict(key, pairs_compose[key], 200000, 0.01)
---
+++ code/graph.py
@@ -0,0 +1,32 @@
+# Author: Luo Xuan
+# This file generate the analysis graph in the doc
+# Run: python3 graph.py
+
+import numpy as np
+import sklearn as sk
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import GradientBoostingRegressor
+
+history = pd.read_excel('history.xlsx')
+predict =pd.read_excel('predict.xlsx')
+f, ax = plt.subplots(4, 4, sharex=True, figsize=(20,10))
+
+predict.plot(title="X benchmark trend Unit: x/d", ax=ax[0][0], color=tuple(tuple(e) for e in np.random.rand(1, 3).tolist()))
+history['TIPS-5Y'].plot(title='history TIPS-5Y', ax=ax[0][1], color=((0.1, 0.2, 0.3)))
+history['TIPS-10Y'].plot(title='history TIPS-10Y', ax=ax[0][2], color=((0.2, 0.1, 0.9)))
+history['TIPS-20Y'].plot(title='history TIPS-20Y', ax=ax[0][3], color=((0.1, 0.27, 0.2)))
+history['TIPS-30Y'].plot(title='history TIPS-30Y', ax=ax[1][0], color=((0.1, 0.6, 0.3)))
+history['TIPS-LONG'].plot(title='history TIPS-LONG', ax=ax[1][1], color=((0.6, 0.2, 0.3)))
+history['UST Bill 1-Y RETURN'].plot(title='history UST Bill 1-Y RETURN', ax=ax[1][2], color=((0.6, 0.7, 0.3)))
+history['UST BILL 10-Y RETURN'].plot(title='history UST BILL 10-Y RETURN', ax=ax[1][3], color=((0.8, 0.4, 0.3)))
+history['SP500'].plot(title='history SP500', ax=ax[2][0], color=((0.1, 0.6, 0.3)))
+history['LIBOR-OVERNIGHT'].plot(title='history LIBOR-OVERNIGHT', ax=ax[2][1], color=((0.2, 0.2, 0.65)))
+history['COMEX-NC-LONG'].plot(title='history COMEX-NC-LONG', ax=ax[2][2], color=((0.1, 0.62, 0.32)))
+history['COMEX-NC-SHORT'].plot(title='history COMEX-NC-NET', ax=ax[2][3], color=((0.71, 0.12, 0.3)))
+history['SPDR:t'].plot(title='history SPDR:t', ax=ax[3][0], color=((0.81, 0.12, 0.43)))
+history['USD/CNY'].plot(title='USD/CNY', ax=ax[3][1], color=((0.1, 0.62, 0.3)))
+
+plt.savefig('x.png')
+plt.show()
